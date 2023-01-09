80b5c7821280a9e1a3bf0a24d6f996be605a8e8e
@hexlet/cli <support@hexlet.io>
2023-01-10 00:15:09 +0300

    submit java-collections-ru/lists

--- java-collections-ru/lists/src/main/java/exercise/App.java
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -7,15 +7,23 @@ import java.util.List;
 // BEGIN
 public class App {
     public static boolean scrabble(String symbols, String word) {
-        List<String> listSymbols = new ArrayList<String>();
-        List<String> listWord = new ArrayList<String>();
-        boolean result = true;
-        for (int item: listWord) {
-            if (!listSymbols.contains(item)) {
+        List<String> symb = new ArrayList<>();
+        List<String> wordl = new ArrayList<>();
+        for (char item: symbols.toCharArray()) {
+            symb.add(Character.toString(item));
+        }
+        for (char item: word.toCharArray()) {
+            wordl.add(Character.toString(item));
+        }
+        for (String item: wordl) {
+            if (!symb.contains(item.toLowerCase())) {
                 return false;
+            } else {
+                symb.remove(item);
+                continue;
             }
         }
-        return result;
+        return true;
     }
 }
 //END
