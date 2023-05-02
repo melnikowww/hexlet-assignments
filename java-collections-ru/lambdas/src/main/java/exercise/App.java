0657d56ac7c4b2de6ef321bcf12042fa7403b090
melnikowww <melnikoffspb@ya.ru>
2023-05-02 13:23:53 +0300

    deploy

--- java-collections-ru/lambdas/src/main/java/exercise/App.java
+++ java-collections-ru/lambdas/src/main/java/exercise/App.java
@@ -3,18 +3,19 @@ package exercise;
 import java.util.Arrays;
 
 // BEGIN
-//public class App {
-//    public static String[][] enlargeArrayImage(String[][] image) {
-//        List<List<String>> imageList = new ArrayList<>();
-//        List<String> subImageList = new ArrayList<>();
-//        for (String[] item: image) {
-//            for (String subItem: item) {
-//                subImageList.add(item);
-//            }
-//            imageList.add(subImageList);
-//        }
-//        imageList.stream()
-//            .forEach(d -> System.out.println(d));
-//    }
-//}
+public class App {
+    public static String[][] enlargeArrayImage(String[][] image) {
+        List<List<String>> imageList = new ArrayList<>();
+        List<String> subImageList = new ArrayList<>();
+        for (String[] item: image) {
+            for (String subItem: item) {
+                subImageList.add(item);
+            }
+            imageList.add(subImageList);
+        }
+        imageList.stream()
+            .forEach(d -> System.out.println(d));
+    }
+
+}
 // END
