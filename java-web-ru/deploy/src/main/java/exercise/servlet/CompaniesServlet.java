8933ee35fec438b6c281800506d78eb1204054c5
@hexlet/cli <support@hexlet.io>
2023-05-02 14:24:17 +0300

    submit java-web-ru/deploy

--- java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
+++ java-web-ru/deploy/src/main/java/exercise/servlet/CompaniesServlet.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.util.List;
-import java.util.stream.Collectors;
 import static exercise.Data.getCompanies;
 
 public class CompaniesServlet extends HttpServlet {
