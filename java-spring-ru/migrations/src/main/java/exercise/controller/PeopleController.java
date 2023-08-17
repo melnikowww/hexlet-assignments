6b55e8c9c4a8ac1a30b51ea4a3289d57ada59b3c
@hexlet/cli <support@hexlet.io>
2023-08-17 15:42:20 +0300

    submit java-spring-ru/migrations

--- java-spring-ru/migrations/src/main/java/exercise/controller/PeopleController.java
+++ java-spring-ru/migrations/src/main/java/exercise/controller/PeopleController.java
@@ -1,9 +1,14 @@
 package exercise.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import jakarta.websocket.server.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -15,24 +20,32 @@ public class PeopleController {
     private final JdbcTemplate jdbc;
 
 
-    @RequestMapping(
-        path = "",
-        method = RequestMethod.POST)
+    @PostMapping
     public void createPerson(@RequestBody Map<String, Object> person) {
         String query = "INSERT INTO person (first_name, last_name) VALUES (?, ?)";
         jdbc.update(query, person.get("first_name"), person.get("last_name"));
     }
 
     // BEGIN
-//    @RequestMapping(
-//        method = RequestMethod.GET,
-//        path = "people/"
-//    )
-////    @GetMapping("people/")
-//    public String getPeople() throws JsonProcessingException {
-//        String query = "SELECT * FROM person";
-//        List<Map<String, Object>> allPeople = jdbc.queryForList(query);
-//        return "";
-//    }
+    @GetMapping(
+        produces = "application/json"
+    )
+    public String getPeople() throws JsonProcessingException {
+        String query = "SELECT * FROM person";
+        List<Map<String, Object>> allPeople = jdbc.queryForList(query);
+        JsonMapper jsonMapper = new JsonMapper();
+        return jsonMapper.writeValueAsString(allPeople);
+    }
+
+    @GetMapping(
+        path = "/{id}",
+        produces = "application/json"
+    )
+    public String getPerson(@PathVariable int id) throws JsonProcessingException {
+        String query = "SELECT * FROM PERSON WHERE id = (?)";
+        Map<String, Object> person = jdbc.queryForMap(query, id);
+        JsonMapper jsonMapper = new JsonMapper();
+        return jsonMapper.writeValueAsString(person);
+    }
     // END
 }
